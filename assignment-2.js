1aa24ca1af46a683ae41ccc41c603db831d0c2c5
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-07-30 22:42:15 +0500

    end of assignment 2

--- assignment-2.js
+++ assignment-2.js
@@ -22,33 +22,64 @@
 // let data = ['saad','anus','muzammil']
 // let index = data.indexOf('anus')
 // console.log(index)
+//we can also use includes method which is shown below
 // Given the following string:
 // let string = 'Pakistan'
 // How you will get the ascii code of the letter at the index 4
 // const result = string.charCodeAt(4)
 // console.log(result)
 // Given an array of filenames, iterate over the array & print true on the console if the file is a txt file
-const files = ['document.txt', 'hello.docx', 'names.xlsx', 'profile.txt', 'sample.ppt']
-const extension = '.txt'
+// const files = ['document.txt', 'hello.docx', 'names.xlsx', 'profile.txt', 'sample.ppt']
+// const extension = '.txt'
 
-console.log(files.includes(extension,0))
 
 // for(let i=0;i<files.length;i++){
-//     let isTxt = files.filter(function(file){
-//        console.log(file.indexOf(extension) !== -1)
-//     })
-// }
+//     let isTxt = files[i].includes(extension)
+//        console.log(isTxt)
+//     }
+
 
 
 
 
 // Take an string as input from the user and print whether the string contains a vowel or not and also tell at which index the vowel occurs at first
+// function getVowel(){
+//     let vowelCount = 0
+//     let getString= prompt('Enter string',"Type here")
+//     let vowel = getString.match(/[aeiou]/gi).join("") 
+//     let index = getString.indexOf(vowel)
+//     console.log(index)
+//     console.log(vowel)
+//     for(let i=0;i<getString.length;i++){
+//     if(getString.charAt(i) === "a" || getString.charAt(i) === "e" || getString.charAt(i) === "i" || getString.charAt(i) === "o" || getString.charAt(i) === "u") {
+//         vowelCount+=1
+        
+
+// }
+//     }
+//     return vowelCount
+// }
+// console.log(getVowel())
+
+
+
+
+
 
 // Given an string replace the character 'a' with 'e' and 'll' with 'oo'
-const stringToChange = 'Hello World as Javascript';
+// const stringToChange = 'Hello World as Javascript';
+// console.log(stringToChange.replace("a","e").replace("a","e").replace("a","e").replace("ll","oo"))
+
 
 // Given an string like this -> '12:00:00'
 // Extract out the hrs, minutes and seconds from it.
+
+const myTime = new Date('12:00:00');
+
+const minutes = myTime.getMinutes();
+const hours = myTime.getHours();
+console.log(minutes)
+console.log(hours)
 
 // Take an string as input from user and also take a number as count and repeat the entered string count times and alert it
 // Example:
