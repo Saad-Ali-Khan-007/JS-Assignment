b39e049dd22baaa7ae186934e809e1f8e9afdf9f
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-01 11:09:22 +0500

    Assignment 7 completed

--- assignment-7.js
+++ assignment-7.js
@@ -2,17 +2,37 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
 // Example:
 // Math.random()
-console.log(Math.random()); // gives a random number between 0 and 1
+// console.log(Math.random()); // gives a random number between 0 and 1
+// Math.abs()//Returns the absolute value of x.
+// Math.sqrt()//gives the square root of number
+// Math.floor()//round off 
 
 // Q2: Take a number input from the user and print its table on the console
-
+//  const getNumber = prompt("Enter the number",0)
+// for (let i=1;i<=10;i++){
+//   console.log(`${getNumber} * ${i} =  ${getNumber*i}`)
+    
+// }
 // Q3: Iterate on the given array using nested for loops and print each element on the console
-const array = new Array(parseInt(Math.random() * 20 + 5))
-  .fill(5)
-  .map(() =>
-    new Array(parseInt(Math.random() * 10))
-      .fill(5)
-      .map(() => (Math.random() * 20).toFixed(2))
-  );
+// const array = new Array(parseInt(Math.random() * 20 + 5))
+//   .fill(5)
+//   .map(() =>
+//     new Array(parseInt(Math.random() * 10))
+//       .fill(5)
+//       .map(() => (Math.random() * 20).toFixed(2))
+//   );
+//   for(i=0;i<array.length;i++){
+//     console.log(array[i])
+//   }
 
 // Create new object and iterate over all the properties and log the property name and its value on the console
+// const person = [
+//   {
+//     name : "Saad Ali Khan",
+//     hasValue : 5 
+//   }
+// ]
+// for(i=0;i<person.length;i++){
+//   console.log(person[i].name)
+//   console.log(person[i].hasValue)
+// }
