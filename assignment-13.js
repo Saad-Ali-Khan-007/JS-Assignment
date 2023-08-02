f6fd888a24f7311a391452adcc905914514a729b
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 11:09:48 +0500

    Assignment 13 completed

--- assignment-13.js
+++ assignment-13.js
@@ -4,31 +4,38 @@
 // You have to extract out the functions json and text from it.
 // call the functions and print all the properties present in the properties object
 
-const someArbitraryApiResponse = {
-  data: {
-    json: () => ({
-      words: ["Hello", "Hi"],
-      properties: {
-        success: true,
-      },
-    }),
-    text: () => ({
-      numbers: [1, 2, 3, 5, 6],
-      properties: {
-        sorted: true,
-        length: 5,
-      },
-    }),
-  },
-};
+// const someArbitraryApiResponse = {
+//   data: {
+//     json: () => ({
+//       words: ["Hello", "Hi"],
+//       properties: {
+//         success: true,
+//       },
+//     }),
+//     text: () => ({
+//       numbers: [1, 2, 3, 5, 6],
+//       properties: {
+//         sorted: true,
+//         length: 5,
+//       },
+//     }),
+//   },
+// };
+// const getJson = someArbitraryApiResponse.data.json
+// console.log(getJson().properties)
+// const getText = someArbitraryApiResponse.data.text
+// console.log(getText().properties)
+
 
 const a = {
   name: "John",
 };
+
 /// ======================
 // Correct the code so that the b.name should not equal to a.name
 
-const b = a;
+const b =Object.assign({},a)//Explanation from youtube 
+https://www.youtube.com/watch?v=JmGJUzNsGFs
 b.name = "Doe";
 
 /// ======================
