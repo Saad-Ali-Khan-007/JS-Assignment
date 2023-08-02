f9df2871c761b79c5abe0516fc19c6d815f08ba6
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 20:55:08 +0500

    assignment 14 and 15 completed

--- assignment-14.js
+++ assignment-14.js
@@ -1,7 +1,22 @@
 // Write the implmentation of the function multiplyNumbers so that the following code should run properly
 
+
+function multiplyNumbers(){
+// console.log(arguments)
+     let a=1
+     for(let i=0 ; i<arguments.length ; i++){
+          a *= arguments[i]
+     }
+     return a
+     
+}
+//https://levelup.gitconnected.com/how-to-write-function-with-n-number-of-parameters-in-javascript-a916de1be7a2//explanation
 console.log(multiplyNumbers(1, 2, 3, 4, 5)); // expected result: 120
 console.log(multiplyNumbers(9, 2, 7)); // expected result: 126
 
 // Use the following api and get the data for all the available countries and provinces (use Fetch)
 const URL = "https://documenter.getpostman.com/view/10808728/SzS8rjbc";
+fetch('https://api.covid19api.com/countries')
+.then((res)=>res.json())
+.then((data)=>console.log(data))
+.catch(error=>console.log('Error:Something went wrong'))
--- assignment-15.js
+++ assignment-15.js
@@ -1,3 +1,33 @@
 // Create a class named LandVehicle which should have 1 property on it 'wheels' and 2 methods (to get and set the value of that property)
+class LandVehicle{
+    constructor(wheels){
+        this._wheels = wheels
+    }
+    get getWheels(){
+        this._wheels
+    }
+    set setWheels(wheels){
+        this._wheels=wheels
+    }
+}
 
 // Create a class named Car which should have 3 properties on it (wheels, doors, engineQuality) inherit this class from the LandVehicle class and implement the get and set methods for doors and engineQuality. Also create a 'printSummary' function to print the sumamry about the car
+class Car extends LandVehicle{
+    constructor(wheels,doors,engineQuality){
+        super(wheels)
+        this._doors=doors
+        this._engineQuality=engineQuality
+    }
+     getAllFields(wheels,doors,engineQuality){
+         this._wheels=wheels
+         this._doors=doors
+         this._engineQuality=engineQuality
+    }
+    printSummary(){
+        console.log(Car)
+    }
+}
+let a = new Car ("Service","Scissor","A1")
+a.getAllFields('local','cashier','5 star')
+console.log(a)
+a.printSummary()
