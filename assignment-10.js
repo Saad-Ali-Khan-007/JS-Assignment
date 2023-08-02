b3f1d8bf7a9018ade59259a248c2596b9faee8a3
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 08:01:22 +0500

    Assignment 10 completed

--- assignment-10.js
+++ assignment-10.js
@@ -1,5 +1,34 @@
 // Create a new promise which will resolve after 5 seconds with a string value
-
+// function getString(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             let output = 'Hey I am Saad Ali Khan'
+//             const error = false
+//             if(!error){
+//                 resolve()
+//             }else{
+//                 reject('Error: Something went wrong')
+//             }
+//             document.body.innerHTML=output
+//         },5000)
+//     })
+// }
+console.log(getString())
 // Create a new promise which will be rejected after 3 seconds with a new Error
-
+function getString(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            let output = 'Hey I am Saad Ali Khan'
+            const error = true
+            if(!error){
+                resolve()
+            }else{
+                reject('Error: Something went wrong')
+            }
+            document.body.innerHTML=output
+        },3000)
+    })
+}
 // Also write the code to handle and print the values after completion of the promises created above
+getString().then().catch(error=>console.log(error))
+
