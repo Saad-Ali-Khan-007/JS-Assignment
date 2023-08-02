778b347dc26edcb70f0c848686dc19130eff03ec
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 09:26:05 +0500

    Assignment 12 completed

--- assignment-12.js
+++ assignment-12.js
@@ -1,2 +1,6 @@
 // Ask the user to provide their first and last name
 // print their full name on the console (use template literals for concatenation)
+const firstName = prompt("Enter your first name","Type Here")
+const lastName = prompt("Enter your last name","Type Here")
+const fullName = `${firstName} ${lastName}`
+console.log(fullName)
