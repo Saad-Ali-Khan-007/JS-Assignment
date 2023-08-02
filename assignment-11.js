7fa0eecde771c9f9d3ee6e575252e13399265425
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 09:16:13 +0500

    Assignment 11 completed

--- assignment-11.js
+++ assignment-11.js
@@ -1,6 +1,29 @@
 // Create two buttons
 // - one to start a counter which will log the current time in the console after every 1 second
 // - second button will be used to stop the timer
+// let start =document.getElementById('btn-start')
+// let stop =document.getElementById('btn-stop')
+// let timerId;
+
+// start.addEventListener('click', function() {
+// 	timerId = setInterval(function() {
+//         let date = new Date;
+// 		console.log(`${date.getMinutes()}:${date.getSeconds()}`);
+//     }, 1000);
+// });
+
+// stop.addEventListener('click', function() {
+// 	clearInterval(timerId);
+// });
+
+
+
 
 // Use fetch to get the data from the following api and print the first 10 results on the console
 // https://jsonplaceholder.typicode.com/todos
+// fetch("https://jsonplaceholder.typicode.com/todos")
+// .then((res)=>res.json())
+// .then((data)=>{
+//     for(let i=0;i<10;i++){
+//     console.log((data[i]))}
+// })
