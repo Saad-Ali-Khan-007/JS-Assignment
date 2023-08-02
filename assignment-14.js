dbfdfa1c322955c0a3e5b7740fff87db3049dbbc
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-02 22:31:07 +0500

    assignment 16 completed

--- assignment-14.js
+++ assignment-14.js
@@ -1,11 +1,20 @@
 // Write the implmentation of the function multiplyNumbers so that the following code should run properly
 
 
-function multiplyNumbers(){
+// function multiplyNumbers(){
+// // console.log(arguments)
+//      let a=1
+//      for(let i=0 ; i<arguments.length ; i++){
+//           a *= arguments[i]
+//      }
+//      return a
+     
+// }
+function multiplyNumbers(...multiplier){
 // console.log(arguments)
-     let a=1
-     for(let i=0 ; i<arguments.length ; i++){
-          a *= arguments[i]
+ let a = 1
+     for(let i =0 ; i<multiplier.length ; i++){
+          a *= multiplier[i]
      }
      return a
      
@@ -15,8 +24,8 @@ console.log(multiplyNumbers(1, 2, 3, 4, 5)); // expected result: 120
 console.log(multiplyNumbers(9, 2, 7)); // expected result: 126
 
 // Use the following api and get the data for all the available countries and provinces (use Fetch)
-const URL = "https://documenter.getpostman.com/view/10808728/SzS8rjbc";
-fetch('https://api.covid19api.com/countries')
-.then((res)=>res.json())
-.then((data)=>console.log(data))
-.catch(error=>console.log('Error:Something went wrong'))
+// const URL = "https://documenter.getpostman.com/view/10808728/SzS8rjbc";
+// fetch('https://api.covid19api.com/countries')
+// .then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch(error=>console.log('Error:Something went wrong'))
--- assignment-16/app.js
+++ assignment-16/app.js
@@ -1,7 +1,12 @@
 // Use the tag based selector to select the button
-
+const btn =document.getElementsByTagName("button")
+console.log(btn)
 // use the id based selector to select the div with id = special
-
+const div = document.getElementById('special')
+console.log(div)
 // use the class based selector to select the paragraphs with the class = para
-
+const para = document.getElementsByClassName('para')
+console.log(para)
 // use the query based selector to select all the elements that have an attribute named 'custom' and its value is set to 'selectable' (hint: revise css attribute based selectors)
+const custom = document.querySelectorAll("p[custom='selectable']")
+console.log(custom)
