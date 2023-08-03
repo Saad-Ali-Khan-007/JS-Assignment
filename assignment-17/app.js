6dfb4321ed4ac177821b640333ae3d50ad30059f
Saad-Ali-Khan-007 <saadalikhan042@gmail.com>
2023-08-03 09:01:10 +0500

    assignment 17 completed

--- assignment-17/app.js
+++ assignment-17/app.js
@@ -6,4 +6,20 @@
 
 // You have to study the codepen given above and implement it for
 // - letter count <= 200
-// - vowels count <= 200
+
+(() => {
+  let max_length = 100;
+  let textarea = document.getElementById("user-text");
+  // set attribute max length
+  textarea.setAttribute("maxlength", max_length);
+
+  textarea.addEventListener("input", function () {
+    let value_length = this.value.length;
+    if (value_length > max_length) {
+      this.value = this.value.substring(0, max_length);
+      value_length = this.value.length;
+    }
+    document.getElementById("char-length").innerText =
+      "Characters remaining " + (max_length - value_length);
+  });
+})();
